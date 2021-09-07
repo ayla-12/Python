1eb7ffbf5ee04452756edfe1fe977566155be8ec
ayla kim <75625296+ayla-12@users.noreply.github.com>
2021-09-07 19:35:04 +0900

    Create quiz10.py

---
+++ quiz10.py
@@ -0,0 +1,28 @@
+'''
+Quiz) 프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드시오
+
+조건 : 모듈 파일명은 byme.py 로 작성
+
+(모듈 사용 예제)
+import byme
+byme.sign()
+
+(출력 예제)
+이 프로그램은 나도코딩에 의해 만들어졌습니다.
+유튜브 : http://youtube.com
+이메일 : nadocoding@gmail.com
+'''
+
+import byme
+byme.sign()
+
+# 영상 풀이법이랑 똑같음!!!!!! 잘풀었어용
+
+'''
+이건 byme.py 파일
+
+def sign():
+    print("이 프로그램은 나도코딩에 의해 만들어졌습니다.")
+    print("유튜브 : http://youtube.com")
+    print("이메일 : nadocoding@gmail.com")
+'''
