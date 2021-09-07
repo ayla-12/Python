e93001d3eb6f62a33090b28b4c4e105dcb0f62a1
ayla kim <75625296+ayla-12@users.noreply.github.com>
2021-09-07 16:34:06 +0900

    Update package.py

--- package.py
+++ package.py
@@ -10,3 +10,12 @@ trip_to.detail()
 from travel import vietnam # travel 패키지 안, vietnam 파일을 불러오세요
 trip_to = vietnam.VietnamPackage()
 trip_to.detail()
+
+from travel import *
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
+
+
+# travel 안에 있는 모든걸 쓰겠다는 건데 오류가 남
+# 개발자가 문법 상에서 공개 범위를 설정을 해줘야하기 때문임
+# __init__ 파일 안에 __all__을 통해서 공개 범위를 설정해 줄 수 있음!
